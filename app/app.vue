a2b9e06a13619b984de5f462c6369f86f0a35e11
alexstapfofficial <alexander_stapf@web.de>
2025-11-13 12:48:44 +0100

    Add default layout wrapper for page rendering

--- app/app.vue
+++ app/app.vue
@@ -11,29 +11,10 @@ useHead({
     lang: 'de'
   }
 })
-const { isLoading } = useLoadingIndicator()
-const appear = ref(false)
-const appeared = ref(false)
-
-
-onMounted(() => {
-  setTimeout(() => {
-    appear.value = true
-    setTimeout(() => {
-      appeared.value = true
-    },0)
-  }, 0)
-})
 </script>
 
 <template>
-  <UApp :toaster="{ expand: false }">
-    <AppHeader />
-
-    <UMain class="relative">
-      <NuxtPage />
-    </UMain>
-
-    <AppFooter />
-  </UApp>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
